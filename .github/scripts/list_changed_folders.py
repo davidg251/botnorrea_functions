b4e40ba9ff44415dc7f22fc68ee32a5d9111ed0a
Not A Number <nan140114@gmail.com>
2022-01-03 21:34:35 -0500

    fix issue

--- .github/scripts/list_changed_folders.py
+++ .github/scripts/list_changed_folders.py
@@ -5,12 +5,17 @@ commit_hash   = os.environ['GITHUB_SHA']
 changed_files_command = f'git diff-tree --no-commit-id --name-only -r {commit_hash}'
 
 def git_diff_tree():
-    return  subprocess.run(changed_files_command.split(" "), capture_output=True).stdout
+    return subprocess.run(
+        changed_files_command.split(" "),
+        capture_output=True
+    ).stdout.decode("utf-8")
 
 def main():
     files_paths  = git_diff_tree().split("\n")
+    print(files_paths)
     files_paths.pop()
-    changed_folders = set(map(lambda elem: elem.split("/")[0], files_paths))
-    return changed_folders.reduce(lambda elem: elem != ".github")
+    changed_folders = set( map(lambda elem: elem.split("/")[0], files_paths) )
+    changed_folders.discard('.github')
+    return changed_folders
 
-main()
+print(main())
